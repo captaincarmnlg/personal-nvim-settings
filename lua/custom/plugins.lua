fe674c2af2e5659d39d235992c54f0e2fa543bd8
Nick K <captaincarmnlgames@gmail.com>
2024-03-01 00:41:06 +0100

    added ensure installs

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -47,11 +47,15 @@ local plugins = {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+    config = function()
+      require("trouble").setup()
+    end
   },
   {
     "nvim-treesitter/nvim-treesitter",
