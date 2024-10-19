fb7c3c40b030b955af5e0c0885ef57722340756f
Nick K <captaincarmnlgames@gmail.com>
2024-10-19 22:37:12 +0200

    makiing dap work

--- lua/custom/configs/mason-nvim-dap.lua
+++ lua/custom/configs/mason-nvim-dap.lua
@@ -4,7 +4,7 @@ M.setup = function(_, opts)
   require("mason-nvim-dap").setup({
     -- A list of adapters to install if they're not already installed.
     -- This setting has no relation with the `automatic_installation` setting.
-    ensure_installed = {},
+    ensure_installed = { "codelldb" },
 
     -- NOTE: this is left here for future porting in case needed
     -- Whether adapters that are set up (via dap) should be automatically installed if they're not already installed.
@@ -14,13 +14,19 @@ M.setup = function(_, opts)
     --   - true: All adapters set up via dap are automatically installed.
     --   - { exclude: string[] }: All adapters set up via mason-nvim-dap, except the ones provided in the list, are automatically installed.
     --       Example: automatic_installation = { exclude = { "python", "delve" } }
-    automatic_installation = false,
+    automatic_installation = true,
 
     -- See below on usage
-    handlers = nil,
+    handlers = {
+      function(config)
+        -- all sources with no handler get passed here
+
+        -- Keep original functionality
+        require('mason-nvim-dap').default_setup(config)
+      end,
+    },
 
   })
 end
 
 return M
-
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -24,7 +24,27 @@ local plugins = {
   -- override plugin configs
   {
     "mfussenegger/nvim-dap",
-    "nvimtools/none-ls.nvim"
+    "nvimtools/none-ls.nvim",
+    "nvim-neotest/nvim-nio"
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
   },
   {
     "williamboman/mason.nvim",
